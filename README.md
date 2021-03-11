4fd48e93f2488e33e68bb2c386e329cfad510004
Gang Li <ligangty@users.noreply.github.com>
2021-03-11 17:09:14 +0800

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+### Indy Archive Service
---
+++ src/main/java/org/commonjava/indy/service/archive/Main.java
@@ -0,0 +1,10 @@
+package org.commonjava.indyservice.archive;
+
+// TODO: this is a generated class, please remove when doing further work.
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello service!" );
+    }
+}
